c61ebfd1f5c2212919e070f7e8d8d715c766d9e3
Kim Hoang <hoanggnk.it@gmail.com>
2025-04-25 07:51:35 +0700

    Up commit checkout branch

--- App.tsx
+++ App.tsx
@@ -6,14 +6,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from 'react-native-paper';
 import { enableScreens } from 'react-native-screens';
 import ResetPassword from './components/ResetPassword';
+import HomeUser from './components/user';
 
 enableScreens();
 const Stack = createNativeStackNavigator();
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={HomeUser} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Create New Account" component={Create_New_Account} />
         <Stack.Screen name="Reset Password" component={ResetPassword} />
