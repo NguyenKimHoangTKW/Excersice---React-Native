2a4c14eabcfbbf65515ee72b2005da8be0a50653
unknown <hoanggnk.it@gmail.com>
2025-04-25 22:13:10 +0700

    Final Practice 2

--- App.tsx
+++ App.tsx
@@ -1,22 +1,16 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './components/LoginScreen';
-import Create_New_Account from './components/CreateNewAccount';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PaperProvider } from 'react-native-paper';
-import { enableScreens } from 'react-native-screens';
-import ResetPassword from './components/ResetPassword';
-
-enableScreens();
+import ContactsNavigator from './screens/routes';
+import OptionsScreen from './screens/Options';
+import React from 'react';
 const Stack = createNativeStackNavigator();
 
-export default function App(){
-  return(
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Create New Account" component={Create_New_Account} />
-        <Stack.Screen name="Reset Password" component={ResetPassword} />
+export default function App() {
+  return (
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName='Lab2'>
+        <Stack.Screen name="Lab2" component={ContactsNavigator} options={{headerShown: false}} />
+        <Stack.Screen name="Options" component={OptionsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
